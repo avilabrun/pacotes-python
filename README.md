b5e61d6c1325ea58131ea58d6e503b38114f6ce6
Bruno <avilabruno@live.com>
2025-05-16 22:10:58 -0300

    criaçao de pacotes

---
+++ README.md
@@ -0,0 +1,2 @@
+# Criação de pacotes em Python
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_name"
+    version="0.0.1",
+    author="Bruno Ávila",
+    author_email="avilabruno@live.com",
+    description=" ",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avilabrun/pacotes-python.git"
+    packages=find_packages(),
+    install_requirements=requirements,
+    python_requires=">=3.9.13"
+)
