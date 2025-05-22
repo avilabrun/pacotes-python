ef120da4f9a6635d41d04a22ab1a9f672f008d17
Bruno <avilabruno@live.com>
2025-05-21 21:21:21 -0300

    estrutura do readme e correção do setup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Criação de pacotes em Python
 
+## Package_name
+
+Description
+
+## Installation
+
+Use 
+
+`pip install package_name`
+
+## Usage
+
+`from package_name.modulo import file.myfunction()`
+
+## Author
+
+Bruno Ávila
+
+## License
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name"
+    name="package_name",
     version="0.0.1",
     author="Bruno Ávila",
     author_email="avilabruno@live.com",
     description=" ",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/avilabrun/pacotes-python.git"
+    url="https://github.com/avilabrun/pacotes-python.git",
     packages=find_packages(),
     install_requirements=requirements,
     python_requires=">=3.9.13"
